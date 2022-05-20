85083eef61f4f1df62443464363f474388c87fa6
jmvillasencio <jmvillasencio@yahoo.com>
2022-05-21 07:24:08 +0800

    added pp files

---
+++ modules/manifests/init.pp
@@ -0,0 +1,30 @@
+class pmy-exer {
+	
+	#install packages
+	$installPack = [ 'vim-enhanced' , 'curl' , 'git' ]
+
+	package { $installPack: ensure => 'installed', }
+
+	#create user
+	user { 'monitor':
+		ensure => 'present',
+		home => '/home/monitor',
+		shell => '/bin/bash'.
+		before => Package[$installPack],
+	}		
+
+	#create directory
+	$directories = [ '/home/monitor' , '/home/monitor/scripts' ]
+
+	file { $directories:
+		ensure => 'directory',,
+		owner => 'root',
+		group => 'root',
+		mode => '0750',
+		before => User['monitor'],
+	}
+
+
+}
+
+	
